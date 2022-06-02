7c8763ce6383c023a17332d529f88164198ed555
kutsu-4ta <yamashita.bus@gmial.com>
2022-06-02 23:41:20 +0900

    一つのコンポーネントタグだけを返す場合はそのまま返してもいいよっていう例

--- react/src/components/MyHeading.js
+++ react/src/components/MyHeading.js
@@ -6,10 +6,13 @@ const StyledMyHeading = yamashita.h1`
   font-size: 36px;
 `;
 
-const MyHeading = (props) => {
-    return (
-        <StyledMyHeading>{props.children}</StyledMyHeading>
-    );
-}
+// const MyHeading = (props) => {
+//     return (
+//         <StyledMyHeading>{props.children}</StyledMyHeading>
+//     );
+// }
+//
+// export default MyHeading;
 
-export default MyHeading;
+// 上の例のように、<StyledMyHeading>以外の要素を使わない場合はそのままStyledMyHeadingを返しても同じ結果になるので、スマートに書ける。
+export default StyledMyHeading;
