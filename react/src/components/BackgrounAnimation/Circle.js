88f1852d866bd5cedc11bd044496af1067e526a8
kutsu-4ta <yamashita.bus@gmial.com>
2022-06-04 04:56:40 +0900

    円のアニメーションとりあえず実装

--- react/src/components/BackgrounAnimation/Circle.js
+++ react/src/components/BackgrounAnimation/Circle.js
@@ -1,4 +1,5 @@
-import React from 'react'
+import React, { useState } from 'react'
+import {CSSTransition} from "react-transition-group";
 
 const containerStyle = {
     position: 'relative',
@@ -9,8 +10,8 @@ const circleStyle = {
     display:'block',
     width: '10rem',
     height: '10rem',
-    border: '0.1rem solid #808080',
-    borderTop: '0.1rem solid #808080',
+    border: '0.1rem solid',
+    borderTop: '0.1rem solid',
     borderRadius: '50%',
     position: 'absolute',
     boxSizing: 'border-box',
@@ -19,10 +20,91 @@ const circleStyle = {
     zIndex: '-1'
 };
 
-export default function StyledCircle() {
+const page = () => {
+    //マウントの状態を管理
+    const [mount, setMount] = useState(true);
+
+    //マウントのオンオフを切り替える
+    const changer = () => {
+        setMount(!mount);
+    };
+
+    const transitionStyles = {
+        entering: {
+            opacity: 1,
+            transition: 'all 1s ease',
+            display:'block',
+            width: '10rem',
+            height: '10rem',
+            border: '0.1rem solid',
+            borderTop: '0.1rem solid',
+            borderRadius: '50%',
+            borderColor: 'red',
+            position: 'absolute',
+            boxSizing: 'border-box',
+            top: '-100px',
+            left: '600px',
+            zIndex: '-1'
+        },
+        exiting: {
+            opacity: 0,
+            transition: 'all 1s ease',
+            display:'block',
+            width: '10rem',
+            height: '10rem',
+            border: '0.1rem solid',
+            borderTop: '0.1rem solid',
+            borderRadius: '50%',
+            position: 'absolute',
+            boxSizing: 'border-box',
+            top: '-100px',
+            left: '400px',
+            zIndex: '-1'
+        },
+        entered: {
+            opacity: 1,
+            transition: 'all 1s ease',
+            display:'block',
+            width: '10rem',
+            height: '10rem',
+            border: '0.1rem solid',
+            borderTop: '0.1rem solid',
+            borderRadius: '50%',
+            borderColor: 'blue',
+            position: 'absolute',
+            boxSizing: 'border-box',
+            top: '-100px',
+            left: '500px',
+            zIndex: '-1'
+        },
+        exited: {
+            opacity: 0,
+            transition: 'all 1s ease',
+            display:'block',
+            width: '10rem',
+            height: '10rem',
+            border: '0.1rem solid',
+            borderTop: '0.1rem solid',
+            borderRadius: '50%',
+            borderColor: 'black',
+            position: 'absolute',
+            boxSizing: 'border-box',
+            top: '-100px',
+            left: '200px',
+            zIndex: '-1'
+        },
+
+    }
     return (
-        <div style={containerStyle}>
-            <span style={circleStyle} />
-        </div>
-    );
-};
+        <>
+            <CSSTransition in={mount} timeout={1400} mountOnEnter={true} appear={true}>
+                {(state) => (
+                    <div style={containerStyle}>
+                        <span style={transitionStyles[state]} />
+                    </div>
+                )}
+            </CSSTransition>
+        </>
+    )
+}
+export default page
