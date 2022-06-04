28eb2ce9929e1e527dc914cda35726ea89e5d0bd
kutsu-4ta <yamashita.bus@gmial.com>
2022-06-04 16:19:46 +0900

    円のアニメーションとりあえず実装 仮の動き

--- react/src/components/BackgrounAnimation/Circle.js
+++ react/src/components/BackgrounAnimation/Circle.js
@@ -6,19 +6,6 @@ const containerStyle = {
     widthL: '3rem',
     height: '3rem'
 };
-const circleStyle = {
-    display:'block',
-    width: '10rem',
-    height: '10rem',
-    border: '0.1rem solid',
-    borderTop: '0.1rem solid',
-    borderRadius: '50%',
-    position: 'absolute',
-    boxSizing: 'border-box',
-    top: '-100px',
-    left: '500px',
-    zIndex: '-1'
-};
 
 const page = () => {
     //マウントの状態を管理
@@ -29,10 +16,18 @@ const page = () => {
         setMount(!mount);
     };
 
-    const transitionStyles = {
+    //状態変化時のコールバック
+    const callBacks = {
+        onEnter: () => console.log("enterです"),
+        onEntered: () => console.log("enteredです"),
+        onExit: () => console.log("exitです"),
+        onExited: () => console.log("exitedです"),
+    };
+
+    const ballOneStyles = {
         entering: {
             opacity: 1,
-            transition: 'all 1s ease',
+            transition: 'all 2s ease',
             display:'block',
             width: '10rem',
             height: '10rem',
@@ -42,28 +37,13 @@ const page = () => {
             borderColor: 'red',
             position: 'absolute',
             boxSizing: 'border-box',
-            top: '-100px',
+            top: '-130px',
             left: '600px',
-            zIndex: '-1'
-        },
-        exiting: {
-            opacity: 0,
-            transition: 'all 1s ease',
-            display:'block',
-            width: '10rem',
-            height: '10rem',
-            border: '0.1rem solid',
-            borderTop: '0.1rem solid',
-            borderRadius: '50%',
-            position: 'absolute',
-            boxSizing: 'border-box',
-            top: '-100px',
-            left: '400px',
             zIndex: '-1'
         },
         entered: {
             opacity: 1,
-            transition: 'all 1s ease',
+            transition: 'all 2s ease',
             display:'block',
             width: '10rem',
             height: '10rem',
@@ -73,13 +53,28 @@ const page = () => {
             borderColor: 'blue',
             position: 'absolute',
             boxSizing: 'border-box',
-            top: '-100px',
+            top: '-130px',
+            left: '503px',
+            zIndex: '-1'
+        },
+        exiting: {
+            opacity: 0,
+            transition: 'all 2s ease',
+            display:'block',
+            width: '10rem',
+            height: '10rem',
+            border: '0.1rem solid',
+            borderTop: '0.1rem solid',
+            borderRadius: '50%',
+            position: 'absolute',
+            boxSizing: 'border-box',
+            top: '-130px',
             left: '500px',
             zIndex: '-1'
         },
         exited: {
             opacity: 0,
-            transition: 'all 1s ease',
+            transition: 'all 2s ease',
             display:'block',
             width: '10rem',
             height: '10rem',
@@ -89,18 +84,91 @@ const page = () => {
             borderColor: 'black',
             position: 'absolute',
             boxSizing: 'border-box',
-            top: '-100px',
+            top: '-130px',
             left: '200px',
             zIndex: '-1'
         },
+    };
 
-    }
+    const ballTwoStyles = {
+        exiting: {
+            opacity: 0,
+            transition: 'all 2s ease',
+            display: 'block',
+            width: '5rem',
+            height: '5rem',
+            border: '0.1rem solid',
+            borderTop: '0.1rem solid',
+            borderRadius: '50%',
+            borderColor: 'yellow',
+            position: 'absolute',
+            boxSizing: 'border-box',
+            top: '-100px',
+            right: '10px',
+            zIndex: '-1'
+        },
+        exited: {
+            opacity: 0,
+            transition: 'all 2s ease',
+            display: 'block',
+            width: '50rem',
+            height: '50rem',
+            border: '0.1rem solid',
+            borderTop: '0.1rem solid',
+            borderRadius: '50%',
+            borderColor: 'yellow',
+            position: 'absolute',
+            boxSizing: 'border-box',
+            top: '-60px',
+            right: '10px',
+            zIndex: '-1'
+        },
+        entering: {
+            opacity: 1,
+            transition: 'all 2s ease',
+            display: 'block',
+            width: '5rem',
+            height: '5rem',
+            border: '0.1rem solid',
+            borderTop: '0.1rem solid',
+            borderRadius: '50%',
+            borderColor: 'blue',
+            position: 'absolute',
+            boxSizing: 'border-box',
+            top: '-110px',
+            right: '300px',
+            zIndex: '-1'
+        },
+        entered: {
+            opacity: 1,
+            transition: 'all 2s ease', // 基本的にはtimeoutと同じにする
+            display: 'block',
+            width: '5rem',
+            height: '5rem',
+            border: '0.1rem solid',
+            borderTop: '0.1rem solid',
+            borderRadius: '50%',
+            borderColor: 'red',
+            position: 'absolute',
+            boxSizing: 'border-box',
+            top: '-50px',
+            right: '330px',
+            zIndex: '-1'
+        },
+    };
+
     return (
         <>
-            <CSSTransition in={mount} timeout={1400} mountOnEnter={true} appear={true}>
+            <CSSTransition
+                in={mount}
+                timeout={2000}
+                mountOnEnter
+                appear={true}
+            >
                 {(state) => (
                     <div style={containerStyle}>
-                        <span style={transitionStyles[state]} />
+                        <span style={ballOneStyles[state]} />
+                        <span style={ballTwoStyles[state]} />
                     </div>
                 )}
             </CSSTransition>
